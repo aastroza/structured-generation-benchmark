abf93a0b825779c2decb05069982a9bce835cbb8
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2024-04-08 08:53:26 -0400

    Update loca.py to add system_prompt

--- eval/local.py
+++ eval/local.py
@@ -13,11 +13,17 @@ def generate(
     schema: str = '''{"name": "calculate_triangle_area", "description": "Calculate the area of a triangle given its base and height.", "parameters": {"type": "object", "properties": {"base": {"type": "number", "description": "The base of the triangle."}, "height": {"type": "number", "description": "The height of the triangle."}, "unit": {"type": "string", "description": "The unit of measure (defaults to 'units' if not specified)"}}, "required": ["base", "height"]}}'''
 ):
 
-    generator = outlines.generate.json(model, schema, whitespace_pattern="")
+    generator = outlines.generate.json(model, schema.strip(), whitespace_pattern="")
 
     print(f"Responding to question: {question}")
     function_call = generator(
-        f"{question}"
+        f""""
+    You are an expert in composing functions. You are given a question and a set of possible functions. 
+    Based on the question, you will need to make one or more function/tool calls to achieve the purpose. 
+    If none of the function can be used, point it out. If the given question lacks the parameters required by the function,
+    also point it out. You should only return the function call in tools call sections.
+    Question: {question}
+    """
     )
     print(f"Response: {function_call}")
     return function_call
